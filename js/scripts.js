277803c1f9649845f980f299a647389da6f281a4
William Watkins <wjwat@onionslice.org>
2022-01-19 11:31:22 -0600

    Refactor code to add utility func
    Co-Authored: Andy Plymate <Andy.Plymate@gmail.com>
    Co-Authored-by: Ryan Bass <theryanbass@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,9 @@
+// Utility Logic
+
+function noInputtedWord(word, text) {
+  return ((text.trim().length === 0) || (word.trim().length === 0));
+}
+
 // Business Logic
 
 function wordCounter(text) {
@@ -15,7 +21,7 @@ function wordCounter(text) {
 }
 
 function numberOfOccurrencesInText(word, text) {
-  if (text.trim().length === 0 || word.trim().length === 0) {
+  if (noInputtedWord(word, text)) {
     return 0;
   }
   const wordArray = text.split(" ");
@@ -29,6 +35,9 @@ function numberOfOccurrencesInText(word, text) {
 }
 
 function boldPassage(word, text) {
+  if (noInputtedWord(word, text)) {
+    return 0;
+  }
   let htmlString = "<p>";
   let textArray = text.split(" ");
   textArray.forEach(function (element, index) {
@@ -44,7 +53,7 @@ function boldPassage(word, text) {
   return htmlString + "</p>";
 }
 
-// UI Log
+// UI Logic
 
 $(document).ready(function () {
   $("form#word-counter").submit(function (event) {
